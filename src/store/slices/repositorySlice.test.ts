ea1969022afb8ed7ff91396df168777ca019c4cf
Ezequiel Perez <ezequielperezpc@gmail.com>
2024-10-21 13:28:24 -0300

    Add test script to pre push hook and add repositorySlice tests

--- src/store/slices/repositorySlice.test.ts
+++ src/store/slices/repositorySlice.test.ts
@@ -1,11 +1,17 @@
+import { AxiosResponse } from 'axios';
+
 import repositoryReducer, {
   deleteRepositories,
   deleteSelectedRepositories,
+  fetchRepositories,
   removeRepositorySelection,
   RepositoryState,
   setRepositorySelected,
   setShowSelectionMode,
 } from './repositorySlice';
+
+import RepositoryApi from '@/api/repository';
+import { Repository } from '@/types';
 
 const initialState: RepositoryState = {
   repositories: [
@@ -37,7 +43,38 @@ const initialState: RepositoryState = {
   showSelectionMode: false,
 };
 
+const repositoriesResponse: Partial<AxiosResponse<Repository[]>> = {
+  data: [
+    {
+      id: 1,
+      name: 'Repo1',
+      checked: false,
+      stargazers_count: 120,
+      owner: {
+        avatar_url: '',
+        login: 'Repo Owner',
+      },
+      html_url: '',
+    },
+    {
+      id: 2,
+      name: 'Repo2',
+      checked: false,
+      stargazers_count: 100,
+      owner: {
+        avatar_url: '',
+        login: 'Repo Owner 2',
+      },
+      html_url: '',
+    },
+  ],
+};
+
 describe('repositorySlice reducers', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should toggle showSelectionMode', () => {
     const newState = repositoryReducer(initialState, setShowSelectionMode());
     expect(newState.showSelectionMode).toBe(true);
@@ -45,7 +82,7 @@ describe('repositorySlice reducers', () => {
 
   it('should toggle the checked prop of a repository by id', () => {
     const newState = repositoryReducer(initialState, setRepositorySelected(1));
-    expect(newState.repositories[0].checked).toBe(true); // The first repo's `checked` should now be true
+    expect(newState.repositories[0].checked).toBe(true);
   });
 
   it('should uncheck all repositories on removeRepositorySelection', () => {
@@ -64,11 +101,25 @@ describe('repositorySlice reducers', () => {
   });
 
   it('should delete only checked repositories on deleteSelectedRepositories', () => {
-    const newState = repositoryReducer(
-      initialState,
-      deleteSelectedRepositories(),
-    );
+    let newState = repositoryReducer(initialState, setRepositorySelected(2));
+    newState = repositoryReducer(newState, setRepositorySelected(3));
+
+    newState = repositoryReducer(newState, deleteSelectedRepositories());
+
     expect(newState.repositories).toHaveLength(1);
     expect(newState.repositories[0].id).toBe(1);
+  });
+
+  it('should handle fetchRepositories asyncThunk', async () => {
+    RepositoryApi.getRepositories = jest.fn().mockResolvedValue({
+      data: repositoriesResponse,
+    });
+
+    const action = await fetchRepositories({
+      searchString: 'React',
+      count: 20,
+    })(jest.fn(), () => initialState, undefined);
+
+    expect(action.payload).toEqual(repositoriesResponse);
   });
 });
